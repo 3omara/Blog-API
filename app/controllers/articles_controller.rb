8a5f29fefdd61bafdf1a80f66cf4df2f745024d7
3omara <youssef.ahmed.ibrahim@outlook.com>
2023-06-09 02:15:40 +0300

    Switched from SQLite to MySQL. Populated Database using faker. Implemented and tested all basic CRUD operations. Set up the routes.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,40 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    articles = Article.order(created_at: :desc);
+    render json: {status: 'SUCCESS', message: 'Loaded articles', data: articles}, status: :ok
   end
   def show
-    @article = Article.find(params[:id])
+    article = Article.find(params[:id])
+    render json: {status: 'SUCCESS', message: 'Loaded article', data: article}, status: :ok
   end
-  def new
-    @article = Article.new
-  end
+  
   def create
-    @article = Article.new(title:"...", body:"...")
-    if @article.save
-      redirect_to @article
+    article = Article.new(article_params)
+    if article.save
+      render json: {status: 'SUCCESS', message: 'Saved article', data: article}, status: :ok
     else
-      render :new, status: :unprocessable_entity
+      render json: {status: 'ERROR', message: 'Article not saved', data: article.errors}, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    render json: {status: 'SUCCESS', message: 'Deleted article', data: article}, status: :ok
+  end
+
+  def edit
+    article = Article.find(params[:id])
+    if article.update(article_params)
+      render json: {status: 'SUCCESS', message: 'Updated article', data: article}, status: :ok
+    else
+      render json: {status: 'ERROR', message: 'Article not updated', data: article.errors}, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.permit(:title, :body)
     end
    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
-  
+  put "/articles/:id", to: "articles#edit"
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
